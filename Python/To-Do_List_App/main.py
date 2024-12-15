4c6420576e917f877ee367b8df2887074d49f9a8
Ikenna Ikwuka <ikenna.n.ikwuka@gmail.com>
2024-12-15 01:48:20 +0100

    made changes to create_task() and app_logic() funtion, implemented try...except block in validating user inputs, add comments for more code clarity

--- Python/To-Do_List_App/main.py
+++ Python/To-Do_List_App/main.py
@@ -1,25 +1,8 @@
-# while len(tasks_list) < 11:
-
-#     save_task = input("Would you like to save your tasks? (y/n)").lower().strip()
-#     if save_task[0] == "y":
-#         try:
-#             with open("Task.txt", "r") as file:
-#                 if len(file.readlines()) >= 10:
-#                     print("The file is full")
-#         except FileNotFoundError:
-#             with open("Task.txt", "a") as file:
-#                 file.writelines(tasks_list)
-#         except Exception as e:
-#             print(f"Unexpected error at {e}")
-#         else:
-#             print("File saved successfully")
-#         finally:
-#             print("Operation carried out. Here are the tasks")
-#             for i in tasks_list:
-#                 print(i)
-#     elif save_task[0] != "n":
-#         print("invalid input")
-#     else:
-#         for i in tasks_list:
-#             print(i)
-#         break
+try:
+    choice = int(input("Hi: "))
+    # if not choice.
+    #     raise Exception("hiiiii")
+except ValueError:
+    print("value hi")
+except Exception as e:
+    print(f"error at {e}")
