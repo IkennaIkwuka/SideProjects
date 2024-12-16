4916a454dab4db9be9cfbc5dd0846868929be06e
Ikenna Ikwuka <ikenna.n.ikwuka@gmail.com>
2024-12-16 02:44:26 +0100

    Adding classes to implement Object Oriented Programming (OOP), Created modules for CRUD operations and utility operations, deleted test.py,

--- Python/To-Do_List_App/test.py
+++
@@ -1,8 +0,0 @@
-try:
-    choice = int(input("Hi: "))
-    # if not choice.
-    #     raise Exception("hiiiii")
-except ValueError:
-    print("value hi")
-except Exception as e:
-    print(f"error at {e}")
