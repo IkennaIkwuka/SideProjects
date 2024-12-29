288de4bf22372539ad6998568ff60d26469dad7b
Ikenna Ikwuka <ikenna.n.ikwuka@gmail.com>
2024-12-30 00:44:01 +0100

    Finished game core functionality

--- Number_Guessing_Game/src/main.py
+++ Number_Guessing_Game/src/main.py
@@ -1,39 +1,56 @@
-from utilities import ngg_utility
+"""
+#TODO: Things to add:
+    A difficulty setting // not added
+"""
+
+# from utilities import ngg_utility
 import random
 
 
 class Game:
     def __init__(self, number_range: int) -> None:
         self.range: int = number_range
+        self.computer_input: int = self.get_computer_input()
+        self.user_input: int = self.get_user_input()
 
-    def game_logic(self)  -> None:
+    def game_logic(self) -> str:
         if self.user_input == self.computer_input:
-            print(
-                f"Congratulations!! you got it {self.computer_input} was the right answer."
-            )
+            return f"Congratulations!! you got it {self.computer_input} was the right answer, your input was {self.user_input}"
+        else:
+            return f"Not the right number {self.computer_input} is the right one, your input was {self.user_input}"
 
-    def user(self) -> int:
+    def get_user_input(self) -> int:
         while True:
             try:
-                self.user_input = int(
+                user_input = int(
                     input("A number has been generated. Guess what it is!: ")
                 )
-                return self.user_input
+                return user_input
             except ValueError:
                 print("Error: Must be a number\n")
 
-    def computer(self) -> int:
-        self.computer_input: int = random.randrange(self.range)
-        return self.computer_input
+    def get_computer_input(self) -> int:
+        computer_input: int = random.randrange(self.range)
+        return computer_input
 
 
 def main() -> None:
     print("\nHi!, welcome to the Number Guessing Game.\n")
     print("What difficulty would you like:\n")
-    
-    game_range: int = ngg_utility.get_game_range()
-    
-    Game(game_range)
+    while True:
+        try:
+            game_range = int(
+                input(
+                    "Please give a number in whose range you'll guess what the correct number is:"
+                )
+            )
+            break
+        except ValueError:
+            print("Error: Must be a number.")
+
+    game = Game(game_range)
+
+    print(game.game_logic())
 
 
 if __name__ == "__main__":
