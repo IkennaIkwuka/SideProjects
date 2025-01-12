5988ba09be0249186ba0b81e77d25ec0d2006541
Ikenna Ikwuka <ikenna.n.ikwuka@gmail.com>
2025-01-12 03:28:16 +0100

    module for the constants

---
+++ ToDoListApp/src/sqlite3_constants.py
@@ -0,0 +1,20 @@
+class Constants:
+    # Constraints
+    PRIMARY_KEY = "PRIMARY KEY"
+    AUTOINCREMENT = "AUTOINCREMENT"
+    NOT_NULL = "NOT NULL"
+    UNIQUE = "UNIQUE"
+    FOREIGN_KEY = "FOREIGN KEY"
+    REFERENCES = "REFERENCES"
+    CHECK = "CHECK"
+    DEFAULT = "DEFAULT"
+
+    # Datatypes
+    INTEGER = "INTEGER"
+    TEXT = "TEXT"
+    REAL = "REAL"
+    BLOB = "BLOB"
+    NULL = "NULL"
+
+
+sql = Constants()
