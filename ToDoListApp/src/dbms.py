4ff898af2e0d7160b06e1d75194ed4c17d2971b0
Ikenna Ikwuka <ikenna.n.ikwuka@gmail.com>
2025-01-14 16:22:59 +0100

    Added syntax validation for insert method

--- ToDoListApp/src/dbms.py
+++ ToDoListApp/src/dbms.py
@@ -1,5 +1,7 @@
-from shlex import join
+from multiprocessing import Value
+from operator import le
 import sqlite3
+from turtle import st
 from typing import Any
 import traceback
 from sqlite3_constants import sqlite3_constraints, sqlite3_datatypes
@@ -16,6 +18,7 @@ class DBMS:
         """
 
         self.db_name = db_name
+        self.column_names = []
         try:
             print(f"Connecting to {db_name}.db", end=" ... ")
             self.connection = sqlite3.connect(f"{db_name}.db")
@@ -35,74 +38,29 @@ class DBMS:
             self.get_traceback()
 
     def create(self, table: str, schema: list[tuple[str, str, list[str]]]):
-        """Create table for db.
+        """Create database table
 
         Args:
-            table (str): Name the table.
-            schema (str): SQL schema for the table.
+            table (str): Table name
+            schema (list[tuple[str, str, list[str]]]): List of tuple of str and list[str] to hold the column's name, types and constraints.
         """
-        self.validate(schema)
+        self.validate_create_table(schema)
 
         table_row = []
         for col_name, col_type, col_constraint in schema:
             col_const = " ".join(col_constraint)
             table_row.append(f"{col_name} {col_type} {col_const}")
 
-        _schema = ",\n\t".join(table_row)
-        query = f"CREATE TABLE IF NOT EXISTS {table} (\n\t{_schema}\n)"
+        sql = ",\n\t".join(table_row)
+        query = f"CREATE TABLE IF NOT EXISTS {table} (\n\t{sql}\n)"
         try:
             print(f"Executing query: \n{query}")
             self.cursor.execute(query)
             print(f"Table '{table}' has been created")
-            self.close()
         except Exception as e:
             print(f"Error: {e}")
 
-    def validate(self, schema: list[tuple[str, str, list[str]]]):
-        for col_name, col_type, col_constraint in schema:
-            try:
-                if col_type not in sqlite3_datatypes:
-                    msg = f"Type '{col_type}' is invalid for column '{col_name}'"
-                    suggestion = self.sql_type_suggestion(col_type)
-                    raise ValueError(f"{msg}\n{suggestion}")
-
-                if col_constraint != "":
-                    continue
-
-                for const in col_constraint:
-                    if const not in sqlite3_constraints:
-                        msg = f"Constraint '{const}' is invalid for column '{col_name}'"
-                        suggestion = self.sql_constraint_suggestion(const)
-                        raise ValueError(f"{msg}\n{suggestion}")
-            except ValueError as e:
-                print(f"ERROR: {e}")
-                raise
-
-    def sql_type_suggestion(self, col_type: str) -> str:
-        suggestions = {"INT": "INTEGER", "REA": "REAL", "TEX": "TEXT", "NUL": "NULL"}
-        _ = suggestions.get(col_type[0:3])
-        return f"Did you mean '{_}'?" if _ is not None else ""
-
-    def sql_constraint_suggestion(self, col_constraint: str) -> str:
-        suggestions = {
-            "PRI": "PRIMARY KEY",
-            "UNI": "UNIQUE",
-            "NOT": "NOT NULL",
-            "AUT": "AUTOINCREMENT",
-            "FOR": "FOREIGN KEY",
-            "REF": "REFERENCES",
-            "CHE": "CHECK",
-            "DEF": "DEFAULT",
-        }
-        _ = suggestions.get(col_constraint[0:3])
-        return f"Did you mean '{_}'?" if _ is not None else ""
-
-    def insert_data(
-        self,
-        table: str,
-        columns: str | list[str],
-        values: list[str],
-    ):
+    def insert_data(self, table: str, schema: list[tuple[list[str], list[str]]]):
         """_summary_
 
         Args:
@@ -110,13 +68,78 @@ class DBMS:
             columns (list[str]): _description_
             values (list[str]): _description_
         """
+        # print(len(schema))
+        # for _ in schema:
+        #     print(f"_:{_}")
+        #     for i in _:
+        #         print(f"i:{i}")
 
-        _columns = ",".join(columns)
-        placeholders = ",".join("?" * len(columns))
-        query = f"INSERT INTO {table} ({_columns}) VALUES ({placeholders})"
+        # print(len(schema))
+        # # box =[]
+        self.validate_insert_data(schema)
 
-        if self.executing(query, values):
-            print("Values inserted successfully...")
+    def validate_insert_data(self, schema:list[tuple[list[str], list[str]]]):
+        columns = []
+        values = []
+        for column_name, column_values in schema:
+            if len(column_name) > 1:
+                _ = ", ".join(column_name)
+                msg = f"Multiple Values '{_}' must only have one value"
+                raise ValueError(msg)
+
+            if not column_name:
+                _ = ", ".join(column_values)
+                msg = f"Column of values '{_}' cannot be empty"
+                raise ValueError(msg)
+
+            for col in column_name:
+                if not isinstance(col, str):
+                    msg = f"Column name '{col}' must be a string"
+                    raise ValueError(msg)
+
+                if not col.strip():
+                    _ = ", ".join(column_values)
+                    msg = f"Column of values '{_}' cannot be empty"
+                    raise ValueError(msg)
+
+                if not col.isupper():
+                    msg = f"'{col}' must be in all caps"
+                    raise ValueError(msg)
+
+                if col not in self.column_names:
+                    msg = f"'{col}' is not a valid column name"
+                    raise ValueError(msg)
+
+                columns.append(col)
+
+            for val in column_values:
+                if not isinstance(val, str):
+                    msg = f"Value name '{val}' must be a string"
+                    raise ValueError(msg)
+
+                values.append(val)
+        return columns, values
+        # for value in values:
+
+        # if column_name not in self.column_names:
+        # #     # box.append(column_name)
+        # #     # print(len(box))
+        # #     # print(box)
+        # print(f"column name:{column_name}")
+        # print(len(column_name))
+        # print(f"columnvalues:{column_values}")
+        # print(len(column_values))
+        #     # if len(box) != len(column_values):
+        #     #     raise ValueError("Not same lenght")
+        # col_name.
+        # print(len(column_values))
+
+        # column = ",".join(columns)
+        # placeholders = ",".join("?" * len(columns))
+        # query = f"INSERT INTO {table} ({column}) VALUES ({placeholders})"
+
+        # if self.executing(query, values):
+        #     print("Values inserted successfully...")
 
     def get_traceback(self):
         print(" Failed\n")
@@ -194,6 +217,46 @@ class DBMS:
         self.executing(query, values)
 
     # Utils
+    def validate_create_table(self, sql: list[tuple[str, str, list[str]]]):
+        for col_name, col_type, col_constraint in sql:
+            self.column_names.append(col_name)
+            try:
+                if col_type not in sqlite3_datatypes:
+                    msg = f"Type '{col_type}' is invalid for column '{col_name}'"
+                    suggestion = self.sql_type_suggestion(col_type)
+                    raise ValueError(f"{msg}\n{suggestion}")
+
+                if not col_constraint:
+                    continue
+
+                for col_const in col_constraint:
+                    if col_const not in sqlite3_constraints:
+                        msg = f"Constraint '{col_const}' is invalid for column '{col_name}'"
+                        suggestion = self.sql_constraint_suggestion(col_const)
+                        raise ValueError(f"{msg}\n{suggestion}")
+            except ValueError as e:
+                print(f"ERROR: {e}")
+                raise
+
+    def sql_type_suggestion(self, col_type: str) -> str:
+        suggestions = {"I": "INTEGER", "R": "REAL", "T": "TEXT", "N": "NULL"}
+        _ = suggestions.get(col_type[0])
+        return f"Did you mean '{_}'?" if _ is not None else ""
+
+    def sql_constraint_suggestion(self, col_constraint: str) -> str:
+        suggestions = {
+            "P": "PRIMARY KEY",
+            "U": "UNIQUE",
+            "N": "NOT NULL",
+            "A": "AUTOINCREMENT",
+            "F": "FOREIGN KEY",
+            "R": "REFERENCES",
+            "C": "CHECK",
+            "D": "DEFAULT",
+        }
+        _ = suggestions.get(col_constraint[0])
+        return f"Did you mean '{_}'?" if _ is not None else ""
+
     def column_name(self):
         while True:
             if not (user_input := input("Name of column: ")):
@@ -239,16 +302,19 @@ if __name__ == "__main__":
     schema = [
         ("ID", "INTEGER", ["PRIMARY KEY", "AUTOINCREMENT"]),
         ("TASKS", "TEXT", ["UNIQUE", "NOT NULL"]),
-        ("USERS", "NULL", [""]),
+        ("USERS", "NULL", []),
     ]
-
-    # db.validate(schema)
+    # db.validate_create_table(schema)
     db.create(table_name, schema)
-    # # print(sqlite3.complete_statement("SELECT foo; "))
-    # # db.insert_data("boom", "tasks", ["hi chiwendu"])
+    # columns = ["tasks", "45"]
+    # # print(len(columns))
+    # values = ["hi chiwendu"]
+    schema = [(["TASKS"], ["hi chiwendu"]), (["USERS"], ["9", "bokk", "4.6"])]
+    # print(len(schema))
+    db.insert_data("users", schema)
     # # db.update_table("users", "tasks = ?", "id = ?", ["nkechi", "4"])
     # # row = db.fetch_table("users")
     # # for _ in row:
     # #     print(_)
     # # db.drop("example")
-    # db.close()
+    db.close()
