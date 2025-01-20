c7012b6a80125f3707bd6258322952ec5ef30209
Ikenna Ikwuka <ikenna.n.ikwuka@gmail.com>
2025-01-20 17:48:05 +0100

    Refactored main method to reduce complexity, improved input validation when taking user score to limit to range of 0 ~ 100. Added better error message for user experience

--- StudentGradingSystem/src/App.java
+++ StudentGradingSystem/src/App.java
@@ -14,36 +14,27 @@ import java.util.Scanner;
 //  * display corresponding grade range based on average
 
 public class App {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final String[] subjects = { "English", "Maths", "Computer Science", "Physics", "Chemistry" };
+
     public static void main(String[] args) {
-        final String[] subjects = { "English", "Maths", "Computer Science", "Physics", "Chemistry" };
-
         ArrayList<Integer> scores = new ArrayList<>();
+        App app = new App();
 
-        try (Scanner scanner = new Scanner(System.in)) {
-
-            for (String subject : subjects) {
-                while (true) {
-                    System.out.print("Provide the score for " + subject + ": ");
-                    try {
-                        int score = scanner.nextInt();
-                        scores.add(score);
-                        break;
-                    } catch (InputMismatchException e) {
-                        System.out.println("Please input a number");
-                        scanner.next();
-                    }
-                }
-            }
+        for (String subject : subjects) {
+            int score = app.getUserScores(subject);
+            scores.add(score);
         }
+
         int sumOfScores = 0;
 
         for (int score : scores) {
             sumOfScores += score;
         }
 
-        double scoreCount = scores.size();
+        int scoreCount = scores.size();
 
-        double average = sumOfScores / scoreCount;
+        double average = (double) sumOfScores / scoreCount;
 
         System.out.println("\nCompiling results...");
         for (int i = 0; i < subjects.length; i++) {
@@ -52,18 +43,37 @@ public class App {
             System.out.println("For " + subject + " you got " + score);
         }
 
-        System.out.println("Meaning you had an average of " + average);
+        System.out.println("\nMeaning you had an average of " + average);
 
         if (average >= 100) {
             System.out.println("Wow!. perfect score");
-        } else if (average >= 70 && average < 100) {
+        } else if (average >= 70) {
             System.out.println("Wow!. A very good score");
-        } else if (average >= 50 && average < 70) {
+        } else if (average >= 50) {
             System.out.println("Meh! Pretty average I guess");
-        } else if (average >= 30 && average < 50) {
+        } else if (average >= 30) {
             System.out.println("You almost made average, don't give up");
-        } else if (average < 30) {
+        } else {
             System.out.println("Bruh!");
+        }
+
+        scanner.close();
+    }
+
+    private int getUserScores(String subject) {
+        while (true) {
+            System.out.print("Provide the score for " + subject + ": ");
+            try {
+                int score = scanner.nextInt();
+                if (score >= 0 && score <= 100) {
+                    return score;
+                }
+                System.out.println("Please input a valid number between 0 ~ 100 not " + score);
+
+            } catch (InputMismatchException e) {
+                System.out.println("Please input a valid whole number");
+                scanner.next();
+            }
         }
     }
 }
