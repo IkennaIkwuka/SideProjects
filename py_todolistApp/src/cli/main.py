e61d27f35e96de6f7bc193a3140e32c02eeaaaae
Ikenna Ikwuka <ikenna.n.ikwuka@gmail.com>
2025-12-11 23:38:31 +0100

    refactored edit_tasks method improving its code logic, radability, and reducing its complexity, converted repeated strings to constants, resolved imports

--- py_todolistApp/src/cli/main.py
+++ py_todolistApp/src/cli/main.py
@@ -1,41 +1,11 @@
 # TODOLIST APP
 
-from pathlib import Path
+# At the top of your file, add:
 import textwrap
-from utils.python import project_path
+from utils.python import interminal_text_editor, project_path, write_to_file, read_file
 
 TASK_FILE = project_path(__file__, "docs", "Tasks.txt")
-
-
-def read_file(file: Path | str):
-    """
-    The function `read_file` reads a file line by line and yields each line as a string.
-
-    :param file: The `file` parameter in the `read_file` function can accept either a `Path` object or a
-    string representing the file path
-    :type file: Path | str
-    """
-    with open(file, "r") as f:
-        for line in f:
-            yield line
-
-
-def write_to_file(file: Path | str, list: list[str]):
-    """
-    The function writes a list of strings to a file, with each string on a new line.
-
-    :param file: The `file` parameter in the `write_to_file` function is the path to the file where you
-    want to write the list of strings. It can be either a `Path` object or a string representing the
-    file path
-    :type file: Path | str
-    :param list: The `list` parameter in the `write_to_file` function is a list of strings that you want
-    to write to the specified file. Each string in the list will be written to the file on a new line
-    :type list: list[str]
-    """
-
-    with open(file, "w") as f:
-        for val in list:
-            f.write(f"{val}\n")
+CLOSING_MESSAGE = "\nClosing... Returning to menu\n"
 
 
 class ToDoListApp:
@@ -66,7 +36,7 @@ class ToDoListApp:
 
             print()
 
-            if user_input in ["Q", "q"]:
+            if user_input.lower() == "q":
                 print("Closing Todo List App, goodbye!...")
                 break
 
@@ -111,8 +81,8 @@ class ToDoListApp:
         while True:
             user_input = input("\nTask to add ('q' to Quit)\n\n>    ").strip()
 
-            if user_input in ["Q", "q"]:
-                print("\nClosing... Returning to menu\n")
+            if user_input.lower() == "q":
+                print(CLOSING_MESSAGE)
                 return
 
             if user_input in self.tasks_list:
@@ -129,18 +99,18 @@ class ToDoListApp:
             return
 
         while True:
-            prompt = input(
-                "\nTask index to remove ('-1' to remove all, '0' to view tasks, 'q' to Quit)\n\n>    "
+            prompt_input = input(
+                "\nIndex of task to remove ('-1' to remove all, '0' to view tasks, 'q' to Quit)\n\n>    "
             ).strip()
 
-            if prompt in ["Q", "q"]:
-                print("\nClosing... Returning to menu\n")
+            if prompt_input.lower() == "q":
+                print(CLOSING_MESSAGE)
                 return
 
             try:
-                user_input = int(prompt)
+                user_input = int(prompt_input)
             except ValueError:
-                print(f"\n'{prompt}' is invalid. Please provide a valid index")
+                print(f"\n'{prompt_input}' is invalid. Please provide a valid index")
                 continue
 
             if user_input == 0:
@@ -163,51 +133,40 @@ class ToDoListApp:
             print(f"\nTask: '{val}' has been removed")
 
     def edit_tasks(self):
-        list_length = len(self.tasks_list)
-
-        if list_length == 0:
-            print("There are no tasks to edit.\n")
+        if len(self.tasks_list) == 0:
+            print("There are no tasks to edit, create some.\n")
             return
 
-        prompt = (
-            "Provide the index of the task you would like to edit ('Q' to quit)\n: "
-        )
-
         while True:
-            user_input = input(prompt).strip()
+            prompt_input = input(
+                "\nIndex of task to edit ('0' to view tasks, 'q' to Quit)\n\n>    "
+            ).strip()
 
-            if user_input == "Q" or user_input == "q":
-                print("Closing 'Edit Tasks' returning to menu...\n")
+            if prompt_input.lower() == "q":
+                print(CLOSING_MESSAGE)
                 return
 
-            if (user_input != "Q" or user_input != "q") and not user_input.isdigit():
-                print(
-                    f"'{user_input}' is invalid. Please give a valid input ('Q' to quit)\n"
-                )
+            try:
+                user_input = int(prompt_input)
+            except ValueError:
+                print(f"\n'{prompt_input}' is invalid. Please provide a valid index")
                 continue
 
-            index = int(user_input)
-
-            if index - 1 not in range(list_length):
-                print(
-                    f"'{index}' is invalid. Please give a valid index 1 ~ {list_length} ('Q' to quit)\n"
-                )
+            if user_input == 0:
+                self.view_tasks()
                 continue
 
-            updated_task = input(
-                f"You are now editing Task: '{index}. {self.tasks_list[index - 1]}' to ...\n: "
+            if user_input not in range(1, len(self.tasks_list) + 1):
+                print(f"\n'{user_input}' is invalid. Please provide a valid index")
+                continue
+
+            updated_task = interminal_text_editor(
+                f"\nEditing Task {user_input}: ", self.tasks_list[user_input - 1]
             )
-            print(
-                f"Task: '{index}. {self.tasks_list[index - 1]}' has been updated to '{updated_task}'\n"
-            )
 
-            self.tasks_list[index - 1] = updated_task
+            self.tasks_list[user_input - 1] = updated_task
 
-            prompt = "Edit another task? ('Q' to Quit)...\n: "
-
-            with open(TASK_FILE, "w") as f:
-                for i in self.tasks_list:
-                    f.write(f"{i}\n")
+            print(f"\nTask {user_input} updated to: '{updated_task}'")
 
 
 # main method to run program
