9a8d05db4d5c26afc6bab5ef7d65b56d534504b6
Ikenna Ikwuka <ikenna.n.ikwuka@gmail.com>
2026-01-09 22:50:33 +0100

    minor refactor removed print statements as that will be handled by main.py

--- pythonprojects/todolistapp/cli/src/io.py
+++ pythonprojects/todolistapp/cli/src/io.py
@@ -15,26 +15,18 @@ class TodoIO:
     def get_tasks(self):
         return self.tasks
 
-    def display_tasks(self):
-        for idx, task in enumerate(self.tasks, start=1):
-            print(f"{idx}. {task}")
-
     def store_task(self, task: str):
         self.tasks.append(task)
-        print("\nTask created.")
         self.save()
 
     def clear_all(self):
         self.tasks.clear()
-        print("\nAll tasks have been deleted.")
         self.save()
 
     def delete_task(self, index: int):
         self.tasks.pop(index - 1)
-        print("\nTask deleted.")
         self.save()
 
     def edit_task_content(self, index: int, new_task: str):
         self.tasks[index - 1] = new_task
-        print("\nTask updated.")
         self.save()
